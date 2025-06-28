c65607d73c203588ba50ccd8f72ba5910714c2fe
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-06-28 22:40:38 +0800

    setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Home from './pages/Home'
+import Gallery from './pages/Gallery'
+import Admin from './pages/Admin'
+import NotFound from './pages/NotFound'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+       <div className='h-screen w-full '>
+      <Navbar />
+      <Routes>
+        <Route
+          path="/"
+          element={  <Home />}
+        />
+        <Route
+          path="/gallery"
+          element={  <Gallery />}
+        />
+         <Route
+          path="/admin"
+          element={  <Admin />}
+        />
+        <Route path="*" 
+        element={<NotFound />} 
+        />
+        {/* Add more routes as needed */}
+      </Routes>
+      <Footer />
+     
+    </div>
   )
 }
 
