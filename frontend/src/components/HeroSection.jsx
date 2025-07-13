ada255c946668ef086a05990e46474dec498fe49
Wajid Ali <94067298+wx-01@users.noreply.github.com>
2025-07-13 07:19:55 -0300

    style(navbar): completed

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="flex  flex-row justify-center  items-center h-full text-white font-bold bg-[url(../public/images/hero.PNG)] bg-cover">
+    <div className="flex  flex-row justify-center  items-center h-full text-white font-bold bg-[url(/images/hero.PNG)] bg-cover">
+      {/*try to make this bg-image an auto scroll type ,,,will work on that later */}
       {/* <div className="p-2 mt-5 md:mr-18 lg:mr-25">
         <h1 className="text-4xl md:text-5xl lg:text-7xl text-text-primary text-shadow-lg text-wrap">
           Sweet moments, freshly baked with
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AlignJustify, MenuIcon, X } from "lucide-react";
 
 const navItems = [
   { name: "Menu", href: "#menu" },
@@ -8,9 +9,36 @@ const navItems = [
 ];
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    // Prevent scroll when mobile menu is open
+    if (isMenuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [isMenuOpen]);
+
   return (
     <nav
-      className={`fixed w-screen z-40 bg-transparent rounded-b-xl pt-5 px-5 md:px-10`}
+      className={`fixed w-screen z-40 bg-transparent rounded-b-xl px-5 md:px-10 transition-all duration-300 ${
+        isScrolled ? `pt-0  ${!isMenuOpen ? "backdrop-blur-md" : ""} ` : "pt-5"
+      }`}
     >
       <div className="container flex items-center justify-between px-0 border-t-3 border-t-fudge-500">
         <a
@@ -19,7 +47,7 @@ const Navbar = () => {
         >
           <h1 className="">FUDGE</h1>
         </a>
-
+        {/*desktop navbar*/}
         <div className="hidden md:flex space-x-2 md:space-x-8 pr-4 md:pr-6 py-0 translate-y-[-8%]">
           {navItems.map((item, key) => (
             <a
@@ -31,8 +59,37 @@ const Navbar = () => {
             </a>
           ))}
         </div>
-        <div className="flex md:hidden">
-          <button className="text-3xl text-fudge-500 agbalumo-regular font-bold flex items-center p-3 pt-1 border-t-0 border-3 border-fudge-500 rounded-b-2xl hover:text-white hover:bg-fudge-500 transition-all ease-in-out duration-300"></button>
+        {/*mobile navbar*/}
+
+        <button
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+          className={`flex md:hidden text-3xl text-fudge-500 agbalumo-regular font-bold items-center p-3 pt-1 rounded-b-2xl hover:text-white hover:bg-fudge-500 transition-all ease-in-out duration-100 translate-y-[-15%] z-50  ${
+            isMenuOpen ? "" : " border-t-0 border-3 border-fudge-500"
+          }`}
+        >
+          {isMenuOpen ? <X /> : <MenuIcon />}{" "}
+        </button>
+
+        <div
+          className={`fixed top-0 left-0 w-full h-screen z-40 bg-background/60 flex flex-col items-center justify-center transition-opacity  duration-300 ease-in-out
+            will-change-[transform,opacity] ${
+              isMenuOpen
+                ? "opacity-100 translate-y-0 pointer-events-auto visible"
+                : "opacity-0 -translate-y-2 pointer-events-none invisible"
+            }`}
+        >
+          <div className="flex flex-col space-y-8 text-xl">
+            {navItems.map((item, key) => (
+              <a
+                key={key}
+                href={item.href}
+                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </nav>
