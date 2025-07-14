837615a8b4990ff3520b78050d573f9d3dba0ac3
sana-developer <sanasaghir979@gmail.com>
2025-07-14 13:33:30 +0500

    feat(hero): add auto-scrolling background effect

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="flex  flex-row justify-center  items-center h-full text-white font-bold bg-[url(/images/hero.PNG)] bg-cover">
+    <div className="flex  flex-row justify-center  items-center h-full text-white font-bold bg-[url(/images/hero.PNG)] bg-cover auto-x-scroll">
       {/*try to make this bg-image an auto scroll type ,,,will work on that later */}
       {/* <div className="p-2 mt-5 md:mr-18 lg:mr-25">
         <h1 className="text-4xl md:text-5xl lg:text-7xl text-text-primary text-shadow-lg text-wrap">
