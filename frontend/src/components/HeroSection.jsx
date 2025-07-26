6dd3f045a44192c3a15ed3efdfddf7c302910672
Wajid-ali-io <94067298+wx-01@users.noreply.github.com>
2025-07-26 16:41:53 +0500

    hero updates

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 
 const HeroSection = () => {
   const scrollRef = useRef(null);
@@ -12,10 +12,7 @@ const HeroSection = () => {
         scrollRef.current.scrollLeft += scrollSpeed;
 
         // Reset scroll position for infinite loop
-        if (
-          scrollRef.current.scrollLeft >=
-          scrollRef.current.scrollWidth / 2
-        ) {
+        if (scrollRef.current.scrollLeft >= scrollRef.current.scrollWidth / 2) {
           scrollRef.current.scrollLeft = 0;
         }
       }
@@ -31,7 +28,7 @@ const HeroSection = () => {
       <div
         ref={scrollRef}
         className="w-full overflow-hidden whitespace-nowrap absolute"
-        style={{ display: 'flex' }}
+        style={{ display: "flex" }}
       >
         <img
           src="/images/hero.PNG"
@@ -47,7 +44,7 @@ const HeroSection = () => {
         />
       </div>
       {/*try to make this bg-image an auto scroll type ,,,will work on that later */}
-      <div className="p-2 mt-5 md:mr-18 lg:mr-25 z-20">
+      {/* <div className="p-2 mt-5 md:mr-18 lg:mr-25 z-20">
         <h1 className="text-4xl md:text-5xl lg:text-7xl text-text-primary text-shadow-lg text-wrap">
           Sweet moments, freshly baked with
           <span className=" text-fudge-800"> love</span>
@@ -59,7 +56,7 @@ const HeroSection = () => {
       <img
         src="../../images/coffe.png"
         className="h-full  right-2  top-15   object-cover z-20"
-      ></img>
+      ></img> */}
     </div>
   );
 };
